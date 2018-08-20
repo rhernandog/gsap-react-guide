9ea1b9787f52e17c9635a626eba037940f727df1
Rodrigo Hernando <rhernandog@gmail.com>
2018-08-20 00:44:25 -0300

    Added the aticle and corrected route in readme to license file.

--- src/components/simple tween/timeline-sequence.js
+++ src/components/simple tween/timeline-sequence.js
@@ -29,8 +29,8 @@ class TimelineSequence extends Component {
 			.set(this.content, { autoAlpha: 1 })// show content div
 			.from(this.head, 0.5, { left: 100, autoAlpha: 0 })
 			.from(this.subhead, 0.5, { left: -100, autoAlpha: 0 }, "-=0.25") // added -0.25 seconds prior to end this.of timeline
-			.from(this.feature, 0.5, { scale: .5, autoAlpha: 0 }, "+=0.5") // added 0.5 seconds after end of timeline
-			.from(this.description, 0.5, { left: 100, autoAlpha: 0 }, "-=0.25")
+			.from(this.feature, 0.5, { scale: .5, autoAlpha: 0 }, "feature") // added 0.5 seconds after end of timeline
+			.from(this.description, 0.5, { left: 100, autoAlpha: 0 }, "feature+=0.25")
 			.staggerFrom(this.icons, 0.2, { scale: 0, autoAlpha: 0 }, 0.1); //animate all icons with 0.1 second stagger
 	}
 
